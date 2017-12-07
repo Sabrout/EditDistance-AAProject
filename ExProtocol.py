662113bdad33b6ac654ed74308a48aac1e7fed96
Sabrout <omarsabrout@gmail.com>
2017-12-07 02:14:43 +0100

    Added Database Parser and Processor

--- ExProtocol.py
+++ ExProtocol.py
@@ -32,7 +32,7 @@ def experiment100():
 
     print("_____________________________________")
     print("CLASSIC DYNAMIC PROGRAMMING ALGORITHM")
-    result = itrator100(normalize, ed.med_classic, s1, s2)
+    result = iterator(10, normalize, ed.med_classic, s1, s2)
     print(" ")
     print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result[0]))
     print("RUNNING TIME :  %s seconds" % np.sum(result[1]))
@@ -40,7 +40,7 @@ def experiment100():
     print(" ")
     print("_________________")
     print("K STRIP ALGORITHM")
-    result2 = itrator100(normalize, ed.med_k, s1, s2, 10)
+    result2 = iterator(10, normalize, ed.med_k, s1, s2, 10)
     print(" ")
     print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result2[0]))
     print("RUNNING TIME :  %s seconds" % np.sum(result2[1]))
@@ -48,29 +48,59 @@ def experiment100():
     print(" ")
     print("_____________________________")
     print("APPROXIMATED GREEDY ALGORITHM")
-    result3 = itrator100(normalize_recursion, ed.med_greedy, s1, s2)
+    result3 = iterator(10, normalize_recursion, ed.med_greedy, s1, s2)
     print(" ")
     print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result3[0]))
     print("RUNNING TIME :  %s seconds" % np.sum(result3[1]))
 
-    plotter(result[1], result2[1], result3[1])
+    plotter_figure1(result[1], result2[1], result3[1])
+
+
+def experiment_k():
+    s1 = robust_generator()
+    s2 = robust_generator()
+
+    print("______________________")
+    print("K STRIP ALGORITHM [5]")
+    result = iterator(50, normalize, ed.med_k, s1, s2, 5)
+    print(" ")
+    print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result[0]))
+    print("RUNNING TIME :  %s seconds" % np.sum(result[1]))
+
+    print(" ")
+    print("______________________")
+    print("K STRIP ALGORITHM [20]")
+    result2 = iterator(50, normalize, ed.med_k, s1, s2, 20)
+    print(" ")
+    print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result2[0]))
+    print("RUNNING TIME :  %s seconds" % np.sum(result2[1]))
+
+    print(" ")
+    print("______________________")
+    print("K STRIP ALGORITHM [45]")
+    result3 = iterator(50, normalize, ed.med_k, s1, s2, 45)
+    print(" ")
+    print("{} {}".format("MINIMUM EDIT DISTANCE AVERAGE:", result3[0]))
+    print("RUNNING TIME :  %s seconds" % np.sum(result3[1]))
+
+    plotter_figure2(result[1], result2[1], result3[1])
 
 
 # RUNTIME CALCULATOR
-def itrator100(function, function2, s1, s2, *args):
+def iterator(head_start, function, function2, s1, s2, *args):
     result = np.zeros(100)
     time = np.zeros(100)
-    for i in range(10, 100):
+    for i in range(head_start, 100):
         temp1, temp2 = ed.calc_runtime(function, function2, s1[i], s2[i], *args)
         time[i] = temp1
         result[i] = temp2[0]
-    return np.average(result), np.delete(time, np.arange(0, 10))
+    return np.average(result), np.delete(time, np.arange(0, head_start))
 
 
 # AVERAGE RUNTIME NORMALIZER
 def normalize(function, s1, s2, *args):
     result = 0
-    time = np.zeros(5)
+    time = np.zeros(100)
     for i in range(0, time.size):
         temp1, temp2 = ed.calc_runtime(function, s1, s2, *args)
         time[i] = temp1
@@ -88,7 +118,7 @@ def normalize_recursion(function, s1, s2, *args):
     return result, np.average(time)
 
 
-def plotter(time, time2, time3):
+def plotter_figure1(time, time2, time3):
     line_classic, = plt.plot(np.arange(10, 100), time, label='Classic DP')
     plt.setp(line_classic, color='r', linewidth=2.0)
     line_k, = plt.plot(np.arange(10, 100), time2, label='K Strip (K=10)')
@@ -102,8 +132,63 @@ def plotter(time, time2, time3):
     plt.show()
 
 
+def plotter_figure2(time, time2, time3):
+    line1, = plt.plot(np.arange(50, 100), time, label='K Strip (K=5)')
+    plt.setp(line1, color='r', linewidth=2.0)
+    line2, = plt.plot(np.arange(50, 100), time2, label='K Strip (K=20)')
+    plt.setp(line2, color='b', linewidth=2.0)
+    line3, = plt.plot(np.arange(50, 100), time3, label='K Strip (K=45)')
+    plt.setp(line3, color='g', linewidth=2.0)
+    plt.legend(loc='upper left', handles=[line1, line2, line3])
+    plt.xlabel('Length of Strings')
+    plt.ylabel('Running Time')
+    plt.title("Time Complexity Analysis")
+    plt.show()
+
+
+####################################################
+# Protein Database Processing
+def protein_database_parser():
+    database = list()
+    file = open("ProteinDatabase.txt", "r")
+    temp = ""
+    read_flag = False
+    for line in file:
+        if not line:
+            # print(1)
+            continue
+        if line[0] == '>' and not read_flag:
+            # print(2)
+            read_flag = True
+            continue
+        if line[0] != '>' and read_flag:
+            # print(3)
+            # [:-2] to remove \n
+            temp += line[:-2]
+            continue
+        if line[0] == '>' and read_flag:
+            # print(4)
+            database.append(temp)
+            # print(temp)
+            temp = ""
+            continue
+    file.close()
+    return database
+
+
+def protein_database_processor(data):
+    result = np.arange(data.__len__())
+    for i in range(0, data.__len__()):
+        result[i] = ed.med_classic(data[0], data[i])[0]
+        # print(result[i])
+    return result
+
 def main():
-    experiment100()
+    # experiment100()
+    # experiment_k()
+    data = protein_database_parser()
+    result = protein_database_processor(data)
+    print(result.max())
 
 
 if __name__ == "__main__":main()
