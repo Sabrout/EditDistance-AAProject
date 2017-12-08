f19ff0baa2138d3482a345e3064d4baf4079c814
Loujainl <33659742+Loujainl@users.noreply.github.com>
2017-12-08 19:52:16 +0100

    Update EditDistance.py
    My Final Partition of The Code

--- EditDistance.py
+++ EditDistance.py
@@ -20,56 +20,153 @@ def init(s1, s2):
         counter += 1
     return m
 
-
-
-
-def calcByRow(x,y):
-    prev = np.arange(0, len(y)+1)
-   # print(prev)
-    curr = np.zeros(len(y)+1)
-   # print(curr)
-    for i in range(1, len(x)+1):
-        for j in range(1, len(y)+1):
-           # if j == 0:
-            #    ins = np.inf
-            #else:
-            ins = curr[j-1] + 1
-
-            dele = prev[j] + 1
-
-           # if j == 0:
-            #    sub = np.inf
-           # else:
-               
-            if x[i-1] == y[j-1]:
-                    print(x[i-1], y[j-1])
-                    sub = prev[j-1]
-            else:
-                    sub = prev[j-1]+1
-            curr[j] = min(ins, dele, sub)
-        prev = copy.deepcopy(curr)
-       # print(curr)
+# Calculating the Edit Distance In Linear Space
+def calcByRow(x, y):
+    prev = np.arange(0, len(y) + 1)
+    curr = np.zeros(len(y) + 1)
+    for i in range(1,len(y)+1):
+        prev[i]= prev[i-1]+1
+    for i in range(1, len(x) + 1):
+            curr[0] += 1
+            for j in range(1, len(y) + 1):
+                ins = curr[j - 1] + 1
+                dele = prev[j] + 1
+                if x[i - 1] == y[j - 1]:
+                    sub = prev[j - 1]
+                else:
+                    sub = prev[j - 1] + 1
+                curr[j] = min(ins, dele, sub)
+            prev = copy.deepcopy(curr)
     return curr
 
 def split(scoreF, scoreR):
-    #scoreF front forward, scoreR Buttop mup Reversed
-  splitIndex = 0
-  # to locate the best partition (part of the solution)
-  minSum = 0
-  for i, (f, r) in enumerate(zip(scoreF, scoreR[::-1])):
-    # calculate the diagonal minimum index
-    #iterating over the scores and their indexes
-    if sum([f,r]) < monSum:
-        min_sum = sum([f,r])
-        splitIndex = i
-  return splitIndex
+    # scoreF front forward, scoreR Buttom up(Reversed)
+    splitIndex = 0
+    # to locate the best partition (part of the solution)
+    minSum = np.inf
+    for i, (f, r) in enumerate(zip(scoreF, scoreR[::-1])):
+        # calculate the diagonal minimum index
+        # iterating over the scores and their indexes
+        if sum([f, r]) < minSum:
+            minSum = sum([f, r])
+            splitIndex = i
+    return splitIndex
 
+# Minimum Edit Distance (MED)
+# Divide and Conquer DP
+def hirschberge(x,y):
+    firstString = ""
+    secondString = ""
+    operations = ""
+    if len(x) <= 2 or len(y) <= 2:
+        r = med_classicdq(x, y)
+        return r
+    else:
+        xmiddle = int(len(x)/2)
+        scoreL = calcByRow(x[:xmiddle], y)
+        scoreR = calcByRow(x[xmiddle:][::-1], y[::-1])
+        ymid = split(scoreL, scoreR)
+        rowLeft, operationsLU, columnUp  = hirschberge(x[:xmiddle], y[:ymid])
+        rowRight, operationsRD, columnDown  = hirschberge(x[xmiddle:], y[ymid:])
+        firstString = rowLeft + rowRight
+        operations = operationsLU + operationsRD
+        secondString = columnUp + columnDown
+    print(firstString)
+    print(operations)
+    print(secondString)
+    editDistance = calcByRow(firstString,secondString)
+    print(editDistance[-1])
+    return firstString, operations, secondString
 
+# CLASSIC DYNAMIC PROGRAMMING ALGORITHM USED FOR DIVIDE AND CONQURE
+def med_classicdq(s1,s2):
+    # INITIALIZATION
+    m = init(s1, s2)
+    for i in range(1, m.shape[0]):
+        for j in range(1, m.shape[1]):
 
+            # first condition : i is an insertion
+            con1 = m[i - 1, j] + 1
 
+            # second condition : j is a deletion
+            con2 = m[i, j - 1] + 1
 
+            # third condition : i and j are a substitution
+            if s1[i - 1] == s2[j - 1]:
+                # if same letters, we add nothing
+                con3 = m[i - 1, j - 1]
+            else:
+                # if different letters, we add one
+                con3 = m[i - 1, j - 1] + 1
 
+            # assign minimum value
+            m[i][j] = min(con1, con2, con3)
 
+    # Alignment
+    zero = 0
+    mm = np.c_[[zero] * len(m[:]), m]
+    mmm = np.r_[[[zero] * len(mm[1, :])], mm]
+    backmatrix = [[' ' for y in range(len(s2) + 2)] for x in range(len(s1) + 2)]
+    backmatrix[1][1] = 0
+
+    for i in range(2, len(s1) + 2):
+        backmatrix[i][0] = s1[i - 2]
+    for j in range(2, len(s2) + 2):
+        backmatrix[0][j] = s2[j - 2]
+
+    for i in range(2, len(s1) + 2):
+        backmatrix[i][1] = '|'
+
+    for j in range(2, len(s2) + 2):
+        backmatrix[1][j] = '-'
+
+    for i in range(2, len(s1) + 2):
+        for j in range(2, len(s2) + 2):
+            vertical = mmm[i - 1][j] + 1
+            horizontal = mmm[i][j - 1] + 1
+            if s1[i - 2] == s2[j - 2]:
+                diagonal = mmm[i - 1][j - 1]
+            else:
+                diagonal = mmm[i - 1][j - 1] + 1
+
+            mindist = min(diagonal, vertical, horizontal)
+            mmm[i][j] = mindist
+
+            if mindist == diagonal:
+                backmatrix[i][j] = 'bn'
+            elif mindist == vertical:
+                backmatrix[i][j] = '|'
+            else:
+                backmatrix[i][j] = '-'
+
+    ss1 = ""
+    ss2 = ""
+    op = ""
+
+    i = len(s1) + 1
+    j = len(s2) + 1
+    while not (i == 1 and j == 1):
+        c = backmatrix[i][j]
+        if c == '|':
+            ss1 += s1[i - 2] + ' '
+            ss2 += '-' + ' '
+            op += ' ' + ' '
+            i = i - 1
+        elif c == 'bn':
+            ss1 += s1[i - 2] + ' '
+            ss2 += s2[j - 2] + ' '
+            if s1[i - 2] == s2[j - 2]:
+                op += '|' + ' '
+            else:
+                op += ' ' + ' '
+            i = i - 1
+            j = j - 1
+        else:
+            ss1 += '-' + ' '
+            ss2 += s2[j - 2] + ' '
+            op += ' ' + ' '
+            j = j - 1
+    return ss1[::-1],op[::-1], ss2[::-1]
 
 # Minimum Edit Distance (MED)
 # CLASSIC DYNAMIC PROGRAMMING ALGORITHM
